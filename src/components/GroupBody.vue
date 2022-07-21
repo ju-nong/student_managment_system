5e3fffeb87b1494c61ebb4bc9178f6f2159f99db
ju-nong <iiiuooyoo@gmail.com>
2022-07-21 17:01:19 +0900

    재사용을 위해 컴포넌트화

---
+++ src/components/GroupBody.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="groupBody" v-if="child.length !== 0">
+        <GroupView :child="child" :pNames="''"></GroupView>
+    </div>
+</template>
+
+<script>
+import { GroupView } from "@views";
+export default {
+    props: { child: Array },
+    components: { GroupView },
+};
+</script>
+
+<style></style>
